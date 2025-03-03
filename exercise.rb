bcf0eaa01c6c25109f88a0630cd1ea13fee7fbfb
Immadi <mizy9@umsystem.edu>
2025-03-03 10:41:38 -0600

    final answers

--- exercise.rb
+++ exercise.rb
@@ -1,20 +1,20 @@
 class Exercise
-    # Assume that "str" is a sequence of words separated by spaces.
-    # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
-    # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
-    def self.marklar(str)
-      str.gsub(/\b[A-Za-z']+\b/) do |word|
-        if word.length > 4
-          if word[0] =~ /[A-Z]/
-            "Marklar" + (word[-1] =~ /[[:punct:]]/ ? word[-1] : "")
-          else
-            "marklar" + (word[-1] =~ /[[:punct:]]/ ? word[-1] : "")
-          end
+  # Assume that "str" is a sequence of words separated by spaces.
+  # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
+  # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
+  def self.marklar(str)
+    str.gsub(/\b[A-Za-z']+\b/) do |word|
+      if word.length > 4
+        if word[0] =~ /[A-Z]/
+          "Marklar" + (word[-1] =~ /[[:punct:]]/ ? word[-1] : "")
         else
-          word
+          "marklar" + (word[-1] =~ /[[:punct:]]/ ? word[-1] : "")
         end
+      else
+        word
       end
     end
+  end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
   # the "nth" term in the sequence
